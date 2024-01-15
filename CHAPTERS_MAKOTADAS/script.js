80ac16a14dfe44685724da52663442db1571484a
AyaYany <155995978+AyaYany@users.noreply.github.com>
2024-01-15 15:29:50 -0600

    Update script.js

--- CHAPTERS_MAKOTADAS/script.js
+++ CHAPTERS_MAKOTADAS/script.js
@@ -25,3 +25,27 @@ function redirectToChapter(chapterId) {
         console.error(`Chapter with ID '${chapterId}' not found.`);
     }
 }
+
+const webtoonContainer = document.getElementById('webtoon-container');
+
+// Replace 'YOUR_GOOGLE_DRIVE_IMAGE_URL' with the actual image URL from Google Drive
+const chapterImages = [
+  'https://drive.google.com/file/d/1OmDjJqyRM8t_98mGXdyzxP39jwr7wFlj/view?usp=drive_link',
+  'https://drive.google.com/file/d/18IeMa4vxBUtbjEQyobMY8noK5v_EZsho/view?usp=drive_link',
+  
+  // Add more image URLs as needed
+];
+
+function loadChapterImages() {
+  chapterImages.forEach((imageUrl, index) => {
+    const imgElement = document.createElement('img');
+    imgElement.src = imageUrl;
+    imgElement.alt = `Chapter ${index + 1}`;
+    imgElement.className = 'chapter-image';
+    webtoonContainer.appendChild(imgElement);
+  });
+}
+
+// Load the chapter images when the page is loaded
+window.addEventListener('load', loadChapterImages);
+
