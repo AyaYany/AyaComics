706f976c778ec6bbde77bb77f8d1e0f8736bda7b
AyaYany <155995978+AyaYany@users.noreply.github.com>
2024-02-10 01:05:35 -0600

    Update makochap.js

--- MAKOTADAS/makochap.js
+++ MAKOTADAS/makochap.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
             }
 
             function loadChapterContent(chapterNumber) {
-                fetch('MAKOTADAS/makochapters.json')
+                fetch('../MAKOTADAS/makochapters.json')
                     .then(response => response.json())
                     .then(data => {
                         var chapters = Object.keys(data);
