2a9a26ca3a8840787438f4fa4cc06d8b26aad038
AyaYany <155995978+AyaYany@users.noreply.github.com>
2024-02-12 23:35:32 -0600

    Update faqlanguage.js

--- faqlanguage.js
+++ faqlanguage.js
@@ -11,7 +11,7 @@ const enData = {
   "episodes": [
     {"parrafo1": "Hi!, I'm Aya Yany, a passionate manga artist. I love to make spicy comics with a touch of random FUN things. In my free time, you can find me doing my webcomic Makoholic, or playing some Fire Emblem :3<br><br>My goal in this life is to finish all the comics i have in my mind, i hope i can make it! YoY"},
 
-    {"parrafo2": "Hi!, I'm Fevy! Makoholic editor and assistant of Aya Yany, IT director. In my free time, you can find enjoying some Blue Protocol and my favourite game, Parasite Eve."},
+    {"parrafo2": "Hi!, I'm Fevy! Makoholic editor and assistant of Aya Yany, IT director. In my free time, you can find enjoying some Blue Archive and my favourite game, Parasite Eve."},
 
     {"parrafo3": "ALL AUDIENCES version of Aya Yany, can be used for non +18 projects."}
   ]
@@ -22,7 +22,7 @@ const esData = {
   "episodes": [
     {"parrafo1": "¡Hao!, Soy Aya Yany, una apasionada dibujante de manga. Adoro hacer mangas picantes con un toque random de cosas 'curiosas'. Mi tiempo libre lo disfruto haciendo mi webcomic Makotadas o jugando Fire Emblem :3<br<br>Mi propósito en esta vida es lograr terminar todos los comics que tengo en mi mente, ojalá pueda lograrlo YoY!"},
 
-    {"parrafo2": "¡Hola!, Soy Fevy! Editor de Makotadas y asistente de Aya Yany, director IT. En mi tiempo libre me encuentras disfrutando de BLue Protocol y de mi juego favorito Parasite Eve."},
+    {"parrafo2": "¡Hola!, Soy Fevy! Editor de Makotadas y asistente de Aya Yany, director IT. En mi tiempo libre me encuentras disfrutando de Blue Archive y mi juego favorito Parasite Eve."},
 
     {"parrafo3": "Verión APTA PARA TODO PÚBLICO de Aya Yany, puede usarse para proyectos que no sean +18."}
   ]
