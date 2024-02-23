4f3ab2ebab662bd4d7f3bf06217f589de0e1b968
AyaYany <155995978+AyaYany@users.noreply.github.com>
2024-02-23 13:18:56 -0600

    Add files via upload

--- changeLanguage.js
+++ changeLanguage.js
@@ -1,16 +1,15 @@
 //Get the buttons for changing language
-
-
 const langButtons = document.querySelectorAll('.itemLang');
 
 // Get the elements that need to be translated
 const paragraphs = document.querySelectorAll('[data-section="episodes"]');
+const banner = document.querySelector('[data-section="banners"]');
 
 // English JSON data
 const enData = {
   "episodes": [
     {
-      "parrafo1": "A couple of broken hearts will find themselves in a very unusual situation.<br>Comedy/Romance/Ecchi<br><br><b>STATUS:</b> on air"
+      "parrafo1": "A couple of broken hearts will find themselves in a very unusual situation.<br>Comedy/Romance/Ecchi<br><br><b>STATUS:</b> ongoing"
     },
     {
       "parrafo2": "It is not advisable to fall in love in a battle royale.<br><br>Action/RPG/romance/comedy/SFW<br><br><b>STATUS:</b> It will start as soon as Makotadas finishes."
@@ -18,17 +17,19 @@ const enData = {
     {
       "parrafo3": "<p>A teacher with no desire to live runs into a student who is difficult to control.<br><br>Hentai/Yuri<br><br><b>STATUS:</b> Need Funds u_u"
     },
-    {
-      "parrafo4": "<p>Random hentai shorts<br><br><b>STATUS:</b> I make them when there is nothing defined for makotadas and somebody"
+    {"parrafo4": "<p>Random hentai shorts<br><br><b>STATUS:</b> I make them when there is nothing defined for makotadas and somebody"
     }
-  ]
+  ],
+  "banner": {
+    "bannertext":"The Following comics are<br><b>made in my spare time.</b><br>if you want to <b>support</b> this page to make more, you can do it for <b>only 1 dollar</b> monthly vía <a href= https://www.patreon.com/aya_Y><b>PATREON</b></a></p>"
+  }
 };
 
 // Spanish JSON data
 const esData = {
   "episodes": [
     {
-      "parrafo1": "Un par de corazones rotos se encontrarán en una situación poco común.<br>Comedia/Romance/Ecchi<br><br><b>ESTADO:</b> en emisión"
+       "parrafo1": "Un par de corazones rotos se encontrarán en una situación poco común.<br>Comedia/Romance/Ecchi<br><br><b>ESTADO:</b> en emisión"
     },
     {
       "parrafo2": "No conviene enamorarse en un battle royale.<br><br>Accion/RPG/romance/comedia/SFW<br><br><b>ESTADO:</b> Se empezará en cuanto termine Makotadas."
@@ -39,7 +40,10 @@ const esData = {
     {
       "parrafo4": "<p>Cortos hentai random<br><br><b>ESTADO:</b> Los hago cuando no hay nada definido para makotadas y somebody"
     }
-  ]
+  ],
+  "banner": {
+    "bannertext": "Los comics que puedes ver aquí,<br><b>son hechos en mis ratos libres.</b><br>si gustas apoyarme a hacer más,<br> puedes hacerlo por <b>solo 1 dolar</b> al mes<br>vía <a href= https://www.patreon.com/aya_Y><b>PATREON</b></a></p>"
+  }
 };
 
 // Function to change the language
@@ -48,10 +52,12 @@ function changeLanguage(lang) {
     paragraphs.forEach((paragraph, index) => {
       paragraph.innerHTML = enData.episodes[index][`parrafo${index + 1}`];
     });
+    banner.innerHTML = enData.banner.bannertext;
   } else if (lang === 'es') {
     paragraphs.forEach((paragraph, index) => {
       paragraph.innerHTML = esData.episodes[index][`parrafo${index + 1}`];
     });
+    banner.innerHTML = esData.banner.bannertext;
   }
 }
 
@@ -63,29 +69,3 @@ langButtons.forEach(button => {
   });
 });
 
-
-
-
-/*const langElement = document.getElementById("lang");
-
-const textsToChange = document.querySelectorAll("[data-section]");
-
-const changeLanguage = async (language) => {
-  const requestJson = await fetch(`./languages/${language}.json`);
-  const jsonData = await requestJson.json();
-
-  for (const textToChange of textsToChange) {
-    const section = textToChange.dataset.section;
-    const value = textToChange.dataset.value;
-
-    textToChange.innerHTML = jsonData[section][value];
-  }
-};
-
-langElement.addEventListener("click", (e) => {
-  if (e.target.dataset.lang) {
-    changeLanguage(e.target.dataset.language);
-  }
-});
-
-*/
